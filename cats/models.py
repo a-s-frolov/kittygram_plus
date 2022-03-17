e1e9a912eca60c53fb17684a7dfeb17fad8f46e3
Андрей Фролов <asfrolov@yandex.ru>
2022-03-18 00:18:13 +0300

    Последние правки перед 2-м релизом

--- cats/models.py
+++ cats/models.py
@@ -1,11 +1,62 @@
 from django.db import models
 
 
-class Cat(models.Model):
-    name = models.CharField(max_length=16)
-    color = models.CharField(max_length=16)
-    birth_year = models.IntegerField()
+CHOICES = (
+    ('Gray', 'Серый'),
+    ('Black', 'Чёрный'),
+    ('White', 'Белый'),
+    ('Ginger', 'Рыжий'),
+    ('Mixed', 'Смешанный'),
+)
 
+
+class Owner(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Achievement(models.Model):
+    name = models.CharField(max_length=64)
 
     def __str__(self):
         return self.name
+
+
+class Cat(models.Model):
+    name = models.CharField(max_length=16)
+    color = models.CharField(
+        max_length=16,
+        choices=CHOICES
+    )
+    birth_year = models.IntegerField()
+    owner = models.ForeignKey(
+        to=Owner,
+        related_name='cats',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+    achievements = models.ManyToManyField(
+        to=Achievement,
+        through='AchievementCat',
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class AchievementCat(models.Model):
+    achievement = models.ForeignKey(
+        to=Achievement,
+        on_delete=models.CASCADE,
+    )
+    cat = models.ForeignKey(
+        to=Cat,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f'{self.achievement} {self.cat}'
